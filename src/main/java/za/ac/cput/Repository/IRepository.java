3f999785e6df19bd54d9aff9ffd99098dd952def
Jayden951 <jaydenjohnson951@gmail.com>
2022-04-04 21:13:25 +0200

    update IRepository

--- src/main/java/za/ac/cput/Repository/IRepository.java
+++ src/main/java/za/ac/cput/Repository/IRepository.java
@@ -1,8 +1,5 @@
 package za.ac.cput.Repository;
 
-public interface IRepository <T, ID>{
-    T create(T t);
-    T read(ID id);
-    T update(T t);
-    boolean delete(ID id);
+public interface IRepository {
+
 }
