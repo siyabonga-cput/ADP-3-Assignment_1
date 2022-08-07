7d962210503e565ba84ea90d08888a307e1cd5fa
Raeesah <raeesahwilliams123@gmail.com>
2022-08-07 22:03:43 +0200

    SubjectDepartment and Transport classes

--- src/main/java/za/ac/cput/domain/StaffDetails/SubjectDepartment.java
+++ src/main/java/za/ac/cput/domain/StaffDetails/SubjectDepartment.java
@@ -1,25 +1,36 @@
-///* SubjectDepartment.java
-//   Entity for the Subject Department
-//   Author: Raeesah Williams (219091498)
-//   Date: 30 March 2022
-//*/
-//
-//package za.ac.cput.Domain.StaffDetails;
-//
-//public class SubjectDepartment
-//{
-//    // constructors
-//    private String subDeptID;
-//    private String subjectID;
-//    private String teacherID;
-//    private String typeOfDepartment;
-//
-//    // getters and setters
-//    public String getSubDeptID()
-//    {
-//        return subDeptID;
-//    }
-//
+/* SubjectDepartment.java
+   Entity for the Subject Department
+   Author: Raeesah Williams (219091498)
+   Date: 30 March 2022
+*/
+
+package za.ac.cput.domain.StaffDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
+public class SubjectDepartment
+{
+    // constructors
+    @NotNull @Id
+    public String subDeptID;
+//    public String subjectID;
+//    public String teacherID;
+    private String typeSubject;
+
+    protected SubjectDepartment(){
+
+    }
+
+    // getters
+    public String getSubDeptID()
+    {
+        return subDeptID;
+    }
+
 //    public String getSubjectID()
 //    {
 //        return subjectID;
@@ -29,93 +40,88 @@
 //    {
 //        return teacherID;
 //    }
-//
-//    public String getTypeOfDepartment()
-//    {
-//        return typeOfDepartment;
-//    }
-//
-//    public void setSubDeptID(String subDeptID) {
-//        this.subDeptID = subDeptID;
-//    }
-//
-//    public void setSubjectID(String subjectID) {
-//        this.subjectID = subjectID;
-//    }
-//
-//    public void setTeacherID(String teacherID) {
-//        this.teacherID = teacherID;
-//    }
-//
-//    public void setTypeOfDepartment(String typeOfDepartment) {
-//        this.typeOfDepartment = typeOfDepartment;
-//    }
-//
-//    // toString
-//    @Override
-//    public String toString()
-//    {
-//        return "SubjectDepartment{" +
-//                "subDeptID=" + subDeptID +
-//                ", subjectID=" + subjectID +
-//                ", teacherID=" + teacherID +
-//                ", typeOfTransport='" + typeOfDepartment + '\'' +
-//                '}';
-//    }
-//
-//    // private constructors
-//    private SubjectDepartment (Builder builder)
-//    {
-//        this.subDeptID = builder.subDeptID;
+
+    public String getTypeSubject()
+    {
+        return typeSubject;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SubjectDepartment that = (SubjectDepartment) o;
+        return subDeptID.equals(that.subDeptID) && typeSubject.equals(that.typeSubject);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(subDeptID, typeSubject);
+    }
+
+    // toString
+
+
+    @Override
+    public String toString() {
+        return "SubjectDepartment{" +
+                "subDeptID='" + subDeptID + '\'' +
+                ", typeSubject='" + typeSubject + '\'' +
+                '}';
+    }
+
+    // private constructors
+    private SubjectDepartment (Builder builder)
+    {
+        this.subDeptID = builder.subDeptID;
 //        this.subjectID = builder.subjectID;
 //        this.teacherID = builder.teacherID;
-//        this.typeOfDepartment = builder.typeOfDepartment;
-//    }
-//
-//    // builder pattern
-//    public static class Builder
-//    {
-//        private String subDeptID;
+        this.typeSubject = builder.typeSubject;
+    }
+
+    // builder pattern
+    public static class Builder
+    {
+        private String subDeptID;
 //        private String subjectID;
 //        private String teacherID;
-//        private String typeOfDepartment;
-//
-//        public Builder setSubDeptID(String subDeptID)
-//        {
-//            this.subDeptID = subDeptID;
-//            return this;
-//        }
-//
-//        public Builder setSubjectID(String subjectID)
+        private String typeSubject;
+
+        public SubjectDepartment.Builder setSubDeptID(String subDeptID)
+        {
+            this.subDeptID = subDeptID;
+            return this;
+        }
+
+//        public SubjectDepartment.Builder setSubjectID(String subjectID)
 //        {
 //            this.subjectID = subjectID;
 //            return this;
 //        }
 //
-//        public Builder setTeacherID(String teacherID)
+//        public SubjectDepartment.Builder setTeacherID(String teacherID)
 //        {
 //            this.teacherID = teacherID;
 //            return this;
 //        }
-//
-//        public Builder setTypeOfDepartment(String typeOfDepartment)
-//        {
-//            this.typeOfDepartment = typeOfDepartment;
-//            return this;
-//        }
-//
-//        public Builder copy(SubjectDepartment subjectDepartment)
-//        {
-//            this.subDeptID = subjectDepartment.getSubDeptID();
+
+        public Builder setTypeSubject(String typeSubject)
+        {
+            this.typeSubject = typeSubject;
+            return this;
+        }
+
+        public Builder copy(SubjectDepartment subjectDepartment)
+        {
+            this.subDeptID = subjectDepartment.getSubDeptID();
 //            this.subjectID = subjectDepartment.getSubjectID();
 //            this.teacherID = subjectDepartment.getTeacherID();
-//            this.typeOfDepartment = subjectDepartment.getTypeOfDepartment();
-//            return this;
-//        }
-//
-//        public SubjectDepartment build()
-//        {
-//            return new SubjectDepartment(this);
-//        }
-//    }
-//}
+            this.typeSubject = subjectDepartment.getTypeSubject();
+            return this;
+        }
+        public SubjectDepartment build()
+        {
+            return new SubjectDepartment(this);
+        }
+    }
+}
